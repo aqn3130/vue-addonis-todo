ecd2bd885026ce0e5dec00140691c48b7e1bc794
Ahmad Qurbanzada <ahmad.qurbanzada@outlook.com>
2019-08-08 14:40:39 +0100

    vuetify initial setup

---
+++ client/src/main.js
@@ -0,0 +1,18 @@
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index';
+import vuetify from './plugins/vuetify';
+
+Vue.config.productionTip = false;
+Vue.use(Vuetify);
+sync(store, router);
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
